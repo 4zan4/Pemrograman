4a58830454b0f637ca3fcb750c5c1e96b541389d
jongpapa <114712169+4zan4@users.noreply.github.com>
2023-09-25 20:37:44 +0700

    Add files via upload

---
+++ Tugas 3.2/Tugas 3.2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Tugas_3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNama = new System.Windows.Forms.Label();
+            this.ok = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelPen = new System.Windows.Forms.Label();
+            this.labelPer = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelNama
+            // 
+            this.labelNama.AutoSize = true;
+            this.labelNama.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.labelNama.Location = new System.Drawing.Point(44, 37);
+            this.labelNama.Name = "labelNama";
+            this.labelNama.Size = new System.Drawing.Size(35, 13);
+            this.labelNama.TabIndex = 0;
+            this.labelNama.Text = "Nama";
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(47, 304);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(296, 23);
+            this.ok.TabIndex = 1;
+            this.ok.Text = "Ok";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Location = new System.Drawing.Point(106, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // labelPen
+            // 
+            this.labelPen.AutoSize = true;
+            this.labelPen.Location = new System.Drawing.Point(44, 82);
+            this.labelPen.Name = "labelPen";
+            this.labelPen.Size = new System.Drawing.Size(60, 13);
+            this.labelPen.TabIndex = 3;
+            this.labelPen.Text = "Pendidikan";
+            // 
+            // labelPer
+            // 
+            this.labelPer.AutoSize = true;
+            this.labelPer.Location = new System.Drawing.Point(44, 120);
+            this.labelPer.Name = "labelPer";
+            this.labelPer.Size = new System.Drawing.Size(55, 13);
+            this.labelPer.TabIndex = 4;
+            this.labelPer.Text = "Pekerjaan";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Pedagang",
+            "PNS",
+            "Dosen",
+            "Swasta",
+            "ABRI"});
+            this.listBox1.Location = new System.Drawing.Point(105, 120);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(238, 95);
+            this.listBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SD",
+            "SMP",
+            "SMA",
+            "Diploma",
+            "Sarjana",
+            "Pascasarjana"});
+            this.comboBox1.Location = new System.Drawing.Point(105, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 226);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(296, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(47, 252);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(296, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(47, 278);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(296, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(403, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labelPer);
+            this.Controls.Add(this.labelPen);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.labelNama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNama;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelPen;
+        private System.Windows.Forms.Label labelPer;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Tugas 3.2/Tugas 3.2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_3._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            var nama = textBox1.Text;
+            var pen = comboBox1.Text;
+            var per = listBox1.Text;
+
+            textBox2.Text = string.Format("Halo {0}", nama);
+            textBox3.Text = string.Format("Pendidikan anda {0} ya?", pen);
+            textBox4.Text = string.Format("Anda seorang {0}, hebat !!!", per);
+        }
+    }
+}
